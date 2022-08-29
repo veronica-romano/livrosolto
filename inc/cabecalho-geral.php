3150df1581d41942f3a0209940c0d0cece5fa7f2
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-08-29 09:52:30 -0300

    inserir livro
    pronto para teste

--- inc/cabecalho-geral.php
+++ inc/cabecalho-geral.php
@@ -3,6 +3,9 @@ use Projeto\Livro;
 use Projeto\Usuario;
 require_once "../vendor/autoload.php";
 ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../imagens/logo-e-favicon/favicon-16x16.png" type="image/x-icon">
     <link rel="stylesheet" href="bootstrap.css">
     <link rel="stylesheet" href="estilo.css">

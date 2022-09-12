331e9b7856fcbebced87916cf5cdcb9c72f6340a
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-09-12 09:18:51 -0300

    Update perfil.php

--- visualizacoes/perfil.php
+++ visualizacoes/perfil.php
@@ -1,32 +1,20 @@
 <?php
 use Projeto\Livro;
+use Projeto\ControleDeAcesso;
 require_once "../vendor/autoload.php";
-require_once "../inc/cabecalho-logado.php";
-$detalhe = new Livro;
-$detalhe->setId($_GET['id']);
-$livro = $detalhe->listarUm();
+$sessao = new ControleDeAcesso;
+if(isset($_SESSION['id'])){
+    require_once "../inc/cabecalho-logado.php";
+    } else {
+    require_once "../inc/cabecalho-geral.php"; 
+    }
 ?>
 
     <title>perfil temporário</title>
 </head>
 <body>
 <article class="row d-flex justify-content-evenly ">
-    <section class="col-6 mb-5 mt-5 card shadow-lg" style="max-width: 30rem;">
-        <div class="col-3"><figure><img src="../imagem/<?=$livro['capa']?>" alt="<?=$livro['titulo']?>"></figure></div>
-        <div class="card-footer">
-            <a class="btn btn-success form-control excluir" id="excluir" href="atualiza-temporario.php?id=<?=$livro['id']?>">Atualizar</a>
-            <a class="btn btn-success form-control" href="exclui-temporario.php?id=<?=$livro['id']?>">Excluir</a>
-        </div>
-    </section>
-    <section class="col-6 mb-5 mt-5 card border border-light" style="max-width: 30rem;">
-        <h3 class="mb-2 text-center"><?=$livro['titulo']?></h3>
-        <h4 class="mb-4 mt-2 text-center"><?=$livro['genero']?> de <?=$livro['autor']?></h4>
-        <h5 class="mb-3 mt-3">Disponibilidade: <?=$livro['diasEntrega']?> , <?=$livro['horariosEntrega']?></h5>
-        <div class="mb-3 mt-3">
-            <h6>Descrição do livro: </h6>
-            <p><?=$livro['descricao']?></p>
-        </div>
-    </section>
+
 </article>
 </body>
 </html>

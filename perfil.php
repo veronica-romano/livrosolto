2983ccb1529ed6c31978914d514d3f7f788885ef
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-09-15 10:04:56 -0300

    Update perfil.php

--- perfil.php
+++ perfil.php
@@ -2,19 +2,14 @@
 use Projeto\Livro;
 use Projeto\ControleDeAcesso;
 use Projeto\Usuario;
-
 require_once "./vendor/autoload.php";
 $livros = new Livro;
 $listaDeLivros = $livros->listar();
-
 $sessao = new ControleDeAcesso;
 $usuario = new Usuario;
 $usuario->setId($_SESSION['id']);
 $dados = $usuario->listarUm();
-
-
 ?> 
-
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,63 +17,25 @@ $dados = $usuario->listarUm();
     <link rel="stylesheet" href="bootstrap.css">
     <link rel="stylesheet" href="perfil.css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-    <title>Perfil </title>
-    
+    <title>Perfil </title>   
 </head>
-<header>
-<nav class="navbar navbar-expand-md text-center ">
-    <div class="container mxe-md-5">
-        <h1><a class="navbar-brand pt-2" href="index.php"><img
-          src="imagens/logo-e-favicon/Logo-sem-fundo-2.png"
-                    alt="Letra L com bordas arredondas seguida de Livro Solto, indicando o logo do site"
-                    width="80px"></a></h1>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse row justify-content-md-end justify-content-sm-center justify-content-center"
-            id="navbarSupportedContent">
-            <div class="col-4">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 hamb">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="index.php"
-                            alt="Página inicial">Início</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="login.php" alt="Link para equipe">Perfil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="login.php" alt="Link para equipe">Anunciar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="login.php" alt="Link para equipe">Procurar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="?sair" alt="Link para equipe">Logout</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</nav>
-</header>
 <body>
- 
+<header>
+  <?php
+  if(isset($_SESSION['id'])){
+    require_once "./inc/cabecalho-logado.php";
+    } else {
+    require_once "./inc/cabecalho-geral.php";
+    }
 
-
-
+  ?>
+</header>
   <!-- Perfil -->
-
-
-
   <div class="container">
     <div class="main-body">
-    
-      
-    
           <div class="row gutters-sm">
             <div class="col-md-4 mb-3">
+              <!-- Card perfil com foto -->
               <div class="card">
                 <div class="card-body">
                   <div class="d-flex flex-column align-items-center text-center">
@@ -92,8 +49,9 @@ $dados = $usuario->listarUm();
                   </div>
                 </div>
               </div>
-            
+            <!-- Fim do card Perfil com foto-->
             </div>
+             <!-- Card Informações do Usuário -->
             <div class="col-md-8">
               <div class="card mb-4">
                 <div class="card-body">
@@ -142,6 +100,7 @@ $dados = $usuario->listarUm();
               </div>
 
             </div>
+            <!-- Fim do card Informações do Usuário -->
           </div>
 
         </div>
@@ -157,7 +116,6 @@ $dados = $usuario->listarUm();
 
 
   <div class="d-grid gap-3">
-
   <div class="p-2 bg-white ">
     <!-- Card Livro 1  -->
             
@@ -173,8 +131,6 @@ $dados = $usuario->listarUm();
       
 
   </div>
-
-
   <div class="p-2 bg-white ">
      <!-- Card livro 2  -->
      <div class="row gutters-sm">
@@ -255,8 +211,7 @@ $dados = $usuario->listarUm();
                             <div class="card-body">
                               <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-warning mr-2"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" fill="currentColor" class="bi bi-book-fill" viewBox="0 0 16 16">
   <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
-</svg></i>Seus livros</h6>
-                             
+</svg></i>Seus livros</h6>                          
                         <div class="container">
                             <div class="row accordion" id="accordion">
                               <div class="col-xl-12">
@@ -268,7 +223,7 @@ $dados = $usuario->listarUm();
                                                     <div class="list_block">
                                                         <div class="list_image">
                                                           <?php
-                                                         // foreach ($listaDeLivros as $livros) {
+                                                         // foreach ($listaRecebe as $livroRecebe) {
                                                             ?>
                                                             <img src="./imagem/em-chamas-min.jpg"
                                                              class="image-fit-contain" alt="img" />
@@ -304,45 +259,18 @@ $dados = $usuario->listarUm();
                             </div>
                         </div>
                         <?php 
-                          //}
+                          //codigo que mostra livros que o usuário da session está como id_usuario_recebe
                           ?>
                             </div>
                           </div>
                         </div>
-                      </div>
-
-                      <!-- Fim card livro 3 -->
+                      </div>                    
   </div>
-
 </div>
-          
-
-
-
-               
-
-
-
-                      
-
   </div>
   </div>
-  </div>
-  
-
-       
-               
-
-  <!-- Fim do perfil -->
-
-
-<!-- Cards livros -->
-
-
-
-
-
-
+  </div>     
+<!-- Fim do perfil -->
 <script src="./bootstrap.bundle.js"></script>
 <script src="./nosso.js"></script>
 </body>
